b5604946a95ab19e19628fc8a768029cc6aacceb
RaymondChen000 <RaymondChen000@gmail.com>
2025-12-05 14:57:47 -0500

    commit

--- main.cpp
+++ main.cpp
@@ -8,9 +8,15 @@ enum class State { Q0, Q1, Q_FINAL };
 
 class PDA{
     public:
+        //create the pda obj
+        PDA() {
+            currentState = State::Q0;
+            stack.push('Z'); 
 
+        }
     private:
-
+        State currentState;
+        std::stack<char> stack;
 };
 
 
