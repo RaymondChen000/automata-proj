1b2befd20088551fbd279cffbfce0ff8fa1c6476
RaymondChen000 <RaymondChen000@gmail.com>
2025-12-05 14:55:27 -0500

    commit

--- main.cpp
+++ main.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
-#include <map>
-#include <set>
 #include <stack>
 #include <string>
-#include <vector>
 using namespace std;
 
 // States
 enum class State { Q0, Q1, Q_FINAL };
-
-// Alphabets
-std::set<char> inputAlphabet;
-std::set<char> stackAlphabet;
-
-// Transition Rule
-struct Transition {
-    State nextState;
-    std::string stackOperation;  // like when you pop 1, then push 'A'
-};
 
 class PDA{
     public:
