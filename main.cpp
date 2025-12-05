f622d156e7cae782d112e10a7ce935a8db6d9afb
RaymondChen000 <RaymondChen000@gmail.com>
2025-12-05 14:54:36 -0500

    commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
-#include <iostream> 
+#include <iostream>
+#include <map>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
 using namespace std;
 
-bool PDA(){
-    bool valid = false;
-    
-    return valid;
-}
+// States
+enum class State { Q0, Q1, Q_FINAL };
+
+// Alphabets
+std::set<char> inputAlphabet;
+std::set<char> stackAlphabet;
+
+// Transition Rule
+struct Transition {
+    State nextState;
+    std::string stackOperation;  // like when you pop 1, then push 'A'
+};
+
+class PDA{
+    public:
+
+    private:
+
+};
+
 
 int main() { 
     return 0;
