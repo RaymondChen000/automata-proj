705b4a855ae2d699bf3947c034bbaba43ebff18f
RaymondChen000 <RaymondChen000@gmail.com>
2025-12-05 15:51:56 -0500

    commut

--- main.cpp
+++ main.cpp
@@ -11,12 +11,33 @@ class PDA{
         //create the pda obj
         PDA() {
             currentState = State::Q0;
-            stack.push('Z'); 
-
+            stack.push('Z'); //initial state
         }
+
+        //return true if valid false if not
+        bool valid(const std::string& s){   
+            //for each char in the input string s
+            for(char c : s){
+                //check if it is valid so far
+                if(step(c) == false){
+                    return false;
+                }
+            }
+        }
+
     private:
         State currentState;
         std::stack<char> stack;
+
+        bool step(char c){
+            if (stack.empty()){
+                return false;
+            }
+
+            char top = stack.top();
+
+
+        }
 };
 
 
